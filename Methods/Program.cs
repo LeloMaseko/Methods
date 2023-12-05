28d40ccc124c8520bbfbdf7ef10d600c6faafbff
A253560 <A253560@LSBSA-CCTCSV3>
2023-12-05 13:53:17 +0200

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void sayHi()
+            {
+                Console.WriteLine("Hi");
+            }
+
+            void sayHiUser(string name)
+            {
+                Console.WriteLine("Hello " + name);
+            }
+
+            void sayHiAge(string name, int age)
+            {
+                Console.WriteLine("Hello " + name + " you are " + age);
+            }
+
+            sayHi();
+            sayHiAge("John", 55);
+            sayHiUser("Evans");
+        }
+    }
+}
